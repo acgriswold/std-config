b1c95890beb97be766dcd8c65ed5e123647bfb74
acgriswold <agriswoldcamp@gmail.com>
2024-02-15 16:19:42 -0600

    feat(key-manager): Add more configuration/ setup for ssh-agent
    NOT FINISHED: can set key for agent that will be killed
    Need flag to grab command to run with setup key
    
    Just run after setting up ssh-agent manually

--- ssh/key-manager.sh
+++ ssh/key-manager.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 # PREREQ: echo $SSH_AGENT_PID   or   ps -auxc | grep ssh-agent   then   eval $(ssh-agent)
-# RUN WITH bash ~/.ssh/key-manager.sh -k {key name} -u {user name}
+# RUN WITH bash ~/.ssh/keys/key-manager.sh -k {key name} -u {user name} -s
+
+setupAgent=false
+user="agriswold@hntb.com"
+key=""
 
 handle_error() {
   usage
+  kill_agent
   exit 1
 }
 
-user="{default user}"
-key=""
+handle_success() {
+  exit 1
+}
+
+kill_agent() {
+  if [ $setupAgent ] && [ "$SSH_AGENT_PID" != "" ]; then
+    eval `ssh-agent -k`
+  fi
+}
+
+print_branding() {
+  echo " _____  _____  _____    _____                               "
+  echo "|   __||   __||  |  |  |     | ___  ___  ___  ___  ___  ___ "
+  echo "|__   ||__   ||     |  | | | || .'||   || .'|| . || -_||  _|"
+  echo "|_____||_____||__|__|  |_|_|_||__,||_|_||__,||_  ||___||_|  "
+  echo "                                             |___|          "
+  echo "                                                            "
+
+
+  # echo ' ______   ______   __  __       __    __   ______   __   __   ______   ______   ______   ______    '
+  # echo '/\  ___\ /\  ___\ /\ \_\ \     /\ "-./  \ /\  __ \ /\ "-.\ \ /\  __ \ /\  ___\ /\  ___\ /\  == \   '
+  # echo '\ \___  \\ \___  \\ \  __ \    \ \ \-./\ \\ \  __ \\ \ \-.  \\ \  __ \\ \ \__ \\ \  __\ \ \  __<   '
+  # echo ' \/\_____\\/\_____\\ \_\ \_\    \ \_\ \ \_\\ \_\ \_\\ \_\\"\_\\ \_\ \_\\ \_____\\ \_____\\ \_\ \_\ '
+  # echo '  \/_____/ \/_____/ \/_/\/_/     \/_/  \/_/ \/_/\/_/ \/_/ \/_/ \/_/\/_/ \/_____/ \/_____/ \/_/ /_/ '
+  # echo '                                                                                                   '
+}
 
 # Function to display script usage
 usage() {
@@ -17,6 +46,14 @@ usage() {
   echo " -h, --help            Display this help message"
   echo " -u, --user            User to assign key to"
   echo " -k, --key             REQUIRED: Name of the generated key (illegal characters will be removed)"
+  echo " -l, --list            List currently registered keys"
+  echo " -s, --setup           EXPERIMENTAL: Setup and configure ssh-agent for current shell session"
+  echo "                                     Future expansions will try and pass in command to run after key is setup"
+
+  if [ "$SSH_AGENT_PID" != "" ]; then
+    echo ""
+    echo "ssh-agent current setup up on process id $SSH_AGENT_PID"
+  fi
 }
 
 has_argument() {
@@ -25,6 +62,25 @@ has_argument() {
 
 extract_argument() {
   echo "${2:-${1#*=}}"
+}
+
+# Function to handle options and arguments
+handle_high_priority_options() {
+  while [ $# -gt 0 ]; do
+    case $1 in
+    -s | --setup*)
+      if [ "$SSH_AGENT_PID" = "" ]; then
+        eval `ssh-agent -s`
+        setupAgent=true
+      else
+        echo "ssh-agent already set up with id $SSH_AGENT_PID"
+        setupAgent=false
+      fi
+
+      ;;
+    esac
+    shift
+  done
 }
 
 # Function to handle options and arguments
@@ -44,7 +100,7 @@ handle_options() {
         handle_error
       fi
 
-      key="$HOME/.ssh/keys/$(extract_argument $@ | sed -e 's/[^A-Za-z0-9._-]/_/g')"
+      key="$HOME/.ssh/$(extract_argument $@ | sed -e 's/[^A-Za-z0-9._-]/_/g')"
 
       shift
       ;;
@@ -58,6 +114,14 @@ handle_options() {
 
       shift
       ;;
+    -l | --list*)
+      ssh-add -l
+      handle_success
+
+      ;;
+    -s | --setup*)
+      shift
+      ;;
     *)
       echo "Invalid option: $1" >&2
       handle_error
@@ -68,6 +132,8 @@ handle_options() {
 }
 
 # Run code
+print_branding
+handle_high_priority_options "$@"
 handle_options "$@"
 
 if [ -z "$key" ]; then
@@ -94,3 +160,6 @@ if [ $? -eq 0 ]; then
 else
   echo "Could not add key"
 fi
+
+# kill created ssh-agent to not leak processes
+kill_agent
